fa8979e3eef76dcbfa45bc3790c60f387956c4c7
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2023-09-25 18:32:02 +0300

    Started work on sparseList class. Implemented add() method.

---
+++ sparseList.h
@@ -0,0 +1,33 @@
+//
+// Created by mykh on 25.09.23.
+//
+
+#ifndef LAB1_4_2_SPARSELIST_H
+#define LAB1_4_2_SPARSELIST_H
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+template<typename T>
+class sparseList {
+private:
+    std::list<std::pair<T, size_t>> list{};
+    size_t capacity{};
+public:
+    void add(T data, size_t index) {
+        for (auto i = list.cbegin(); i != list.cend(); i++) {
+            if (i->second == index) {
+                throw std::invalid_argument("Error! Element with this index alraedy in list!");
+            }
+        }
+        list.push_back(std::make_pair(data, index));
+        if (index >= capacity) {
+            capacity = index + 1;
+        }
+    }
+
+};
+
+
+#endif //LAB1_4_2_SPARSELIST_H
