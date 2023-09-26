29d337ec4c8180316aebe315ecaf15aa0d809f85
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2023-09-26 15:17:25 +0300

    Fixed incorrect return type for overloading << operator for vector. Moved body before the class declaration.

--- sparseList.h
+++ sparseList.h
@@ -10,6 +10,19 @@
 #include <vector>
 
 template<typename T>
+std::ostream &operator<<(std::ostream &out, std::vector<T> vector) {
+    out << "[";
+    for (auto i = 0; i < vector.size(); i++) {
+        out << vector[i];
+        if (i < vector.size() - 1) {
+            out << ",";
+        }
+    }
+    out << "]";
+    return out;
+}
+
+template<typename T>
 class sparseList {
 private:
     std::list<std::pair<T, size_t>> list{};
@@ -116,21 +129,10 @@ public:
         return a.second < b.second;
     }
 
-    friend void operator<<(std::ostream &out, std::vector<T>);
+    friend std::ostream &operator<<(std::ostream &out, std::vector<T> vector);
 };
 
-template<typename T>
-std::ostream &operator<<(std::ostream &out, std::vector<T> vector) {
-    out << "[";
-    for (auto i = 0; i < vector.size(); i++) {
-        out << vector[i];
-        if(i<vector.size()-1) {
-            out << ",";
-        }
-    }
-    out << "]";
-    return out;
-}
+
 
 
 #endif //LAB1_4_2_SPARSELIST_H
