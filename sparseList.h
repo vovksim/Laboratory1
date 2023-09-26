a747280bc67244668fb89fe59d353c4d15c4f682
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2023-09-26 15:51:38 +0300

    Added helper.h instead of in-file operator<< overloading realization.

--- sparseList.h
+++ sparseList.h
@@ -8,19 +8,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
-
-template<typename T>
-std::ostream &operator<<(std::ostream &out, std::vector<T> vector) {
-    out << "[";
-    for (auto i = 0; i < vector.size(); i++) {
-        out << vector[i];
-        if (i < vector.size() - 1) {
-            out << ",";
-        }
-    }
-    out << "]";
-    return out;
-}
+#include "helper.h"
 
 template<typename T>
 class sparseList {
