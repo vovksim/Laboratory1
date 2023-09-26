94bfbf6a7a72196234a3bf3fe75163ab58bc0ba7
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2023-09-26 15:37:49 +0300

    Filled sparseMatrix with fields.

--- sparseMatrix.h
+++ sparseMatrix.h
@@ -4,8 +4,13 @@
 
 #ifndef LAB1_4_2_SPARSEMATRIX_H
 #define LAB1_4_2_SPARSEMATRIX_H
-class sparseMatrix {
+#include <vector>
 
+template<typename A>
+class sparseMatrix {
+    std :: size_t capacity{};
+    std :: vector<std::tuple<std::size_t ,std::size_t ,A>> matrix{};
+    A defaultValue{};
 };
 
 #endif //LAB1_4_2_SPARSEMATRIX_H
