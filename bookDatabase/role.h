d4cd5760c1cb989a96efc1d38649c8ec7d789f67
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2023-10-10 20:32:10 +0300

    Created enum class role, which will represent the level of participation in a book for character.

---
+++ bookDatabase/role.h
@@ -0,0 +1,14 @@
+//
+// Created by mykh on 08.10.23.
+//
+
+#ifndef LAB1_4_2_ROLE_H
+#define LAB1_4_2_ROLE_H
+
+enum class role {
+    primary,
+    secondary,
+    tertiary
+};
+
+#endif //LAB1_4_2_ROLE_H
